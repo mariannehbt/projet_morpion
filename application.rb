ae6f74a673082aeb6ff7f25a114ba306864594a8
mariannehbt <marianne.heribert@gmail.com>
2019-10-17 23:05:20 +0200

    commit no.2

--- application.rb
+++ application.rb
@@ -8,6 +8,7 @@ require_relative 'lib/board'
 require_relative 'lib/board_case'
 require_relative 'lib/show'
 
+system('clear')
 my_game = Game.new
 
 Show.new.show_board(my_game.my_board.board_array)
--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 class Game
 
-  #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
-  attr_accessor :my_board, :players_array
-    #le current_player (égal à un objet Player),
+  #TO DO : la classe a plusieurs attr_accessor: le @current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
+  attr_accessor :my_board, :players_array, :current_player
+    #le @current_player (égal à un objet Player),
     #le status (en cours, nul ou un objet Player s'il gagne),
     #et un array contenant les 2 joueurs.
 
-  #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un current_player
+  #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un @current_player
   def initialize
     @players_array = []
     # Création des 2 joueurs
@@ -39,7 +39,7 @@ class Game
     @my_board.board_array << BoardCase.new("C3")
 
     #met le status à "on going"
-    #défini un current_player
+    #défini un @current_player
   end
 
   #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
@@ -50,47 +50,81 @@ class Game
   #1) demande au bon joueur ce qu'il souhaite faire
   #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
   def play_turn
-    @players_array.each do |player|
-      puts "#{player.name}, que souhaites-tu faire ?"
+    @current_player = @players_array[rand(0..1)]
+    while !victory? && !drawn_game?
+      puts "#{@current_player.name}, choisis un chiffre entre 0 et 8 ?"
       print "> "
-      player_choice = gets.chomp.to_i
+      player_choice = Integer(gets) rescue false
+
+      while player_choice == false || player_choice < 0 || player_choice > 8
+        puts "Choisis un chiffre entre 0 et 8 stp !"
+        print "> "
+        player_choice = Integer(gets) rescue false
+      end
 
       while @my_board.board_array[player_choice].case_symbol != nil
         puts "Choisis une case vide stp !"
         print "> "
-        player_choice = gets.chomp.to_i
+        player_choice = Integer(gets) rescue false
       end
 
-      @my_board.board_array[player_choice].case_symbol = player.symbol
-
+      @my_board.board_array[player_choice].case_symbol = @current_player.symbol
       my_display = Show.new.show_board(@my_board.board_array)
+      @current_player = (@players_array - [@current_player])[0]
+    end
+
+    if victory? then puts "#{(@players_array - [@current_player])[0].name} tu as gagné petit malin !"
+    elsif drawn_game? then puts "Match nul"
     end
   end
 
   #méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board)
   #Elle affiche le plateau,
   #demande au joueur ce qu'il joue,
-
   #vérifie si un joueur a gagné,
   #passe au joueur suivant si la partie n'est pas finie.
 
   #TO DO : une méthode qui vérifie le plateau et indique s'il y a un vainqueur ou match nul
   def victory?
-board_array.each |case|
-    if case.symbol == "x"
-      then x_board << case.id
-    end
-    if case_symbol == "o"
-    then o_board << case.id
+    winning_combinaisons =
+    [["A1", "A2", "A3"],
+    ["B1", "B2", "B3"],
+    ["C1", "C2", "C3"],
+    ["A1", "B1", "C1"],
+    ["A2", "B2", "C2"],
+    ["A3", "B3", "C3"],
+    ["A3", "A2", "A1"],
+    ["B3", "B2", "B1"],
+    ["C3", "C2", "C1"],
+    ["C1", "B1", "A1"],
+    ["C2", "B2", "A2"],
+    ["C3", "B3", "A3"],
+    ["A1", "B2", "C3"],
+    ["C1", "B2", "A3"],
+    ["C3", "B2", "A1"],
+    ["A3", "B2", "C1"]]
+
+    @x_board = []
+    @o_board = []
+
+    @my_board.board_array.each do |cells|
+      if cells.case_symbol == "x"
+        then @x_board << cells.case_id
+      end
+      if cells.case_symbol == "o"
+        then @o_board << cells.case_id
+      end
     end
 
-  if  
-  # une méthode qui vérifie le plateau et
-  # indique s'il y a un vainqueur ou match nul
+    winning_combinaisons.any? { |combinaisons| (combinaisons - @x_board) == [] || (combinaisons - @o_board) == [] }
 
+    # une méthode qui vérifie le plateau et
+    # indique s'il y a un vainqueur ou match nul
   end
 
-
+  def drawn_game?
+    !victory? && @x_board.length + @o_board.length == 9
+  end
 
   # TO DO : relance une partie en initialisant un nouveau board mais en gardant les mêmes joueurs.
   def new_round
